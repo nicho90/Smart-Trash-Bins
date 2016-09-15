69099315f58b6d16098ffa1171b37ab19272424e
Flugmango <b_stoe03@uni-muenster.de>
2016-09-15 13:47:33 +0200

    little canges

--- public/js/map.js
+++ public/js/map.js
@@ -23,15 +23,10 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 // return div;
 // };
 
-// Creates a red marker with the coffee icon
+// Create trash bin markers
 var redMarker = L.AwesomeMarkers.icon({
     icon: 'trash',
     markerColor: 'red'
-});
-
-var darkredMarker = L.AwesomeMarkers.icon({
-    icon: 'trash',
-    markerColor: 'darkred'
 });
 
 var orangeMarker = L.AwesomeMarkers.icon({
@@ -40,42 +35,136 @@ var orangeMarker = L.AwesomeMarkers.icon({
 });
 
 var greenMarker = L.AwesomeMarkers.icon({
-    icon: 'folder-close',
+    icon: 'trash',
+    markerColor: 'green'
+});
+// Create GelberSack markers
+
+L.AwesomeMarkers.Icon.prototype.options.prefix = 'fa';
+
+var redSack = L.AwesomeMarkers.icon({
+    icon: 'recycle',
+    markerColor: 'red'
+});
+
+var orangeSack = L.AwesomeMarkers.icon({
+    icon: 'recycle',
+    markerColor: 'orange'
+});
+
+var greenSack = L.AwesomeMarkers.icon({
+    icon: 'recycle',
     markerColor: 'green'
 });
 
-// L.AwesomeMarkers.Icon.prototype.options.prefix = 'glyphicon';
+// Create Papier markers
 
-var whiteMarker = L.AwesomeMarkers.icon({
-    icon: 'folder-close',
-    markerColor: 'white',
-    iconColor: 'black'
+L.AwesomeMarkers.Icon.prototype.options.prefix = 'fa';
+
+var redPaper = L.AwesomeMarkers.icon({
+    icon: 'file-o',
+    markerColor: 'red'
 });
 
-L.marker([51.962377, 7.625153], { icon: darkredMarker }).addTo(map)
-    .bindPopup('Dieser Mülleimer ist komplett voll!');
-L.marker([51.962377, 7.624153], { icon: redMarker }).addTo(map)
-    .bindPopup('Dieser Mülleimer ist fast voll!');
-L.marker([51.961377, 7.625153], { icon: orangeMarker }).addTo(map)
-    .bindPopup('Dieser Mülleimer ist mäßig gefüllt!');;
-L.marker([51.962377, 7.626153], { icon: greenMarker }).addTo(map)
-    .bindPopup('Dieser Mülleimer ist nur ein wenig gefüllt!');
-L.marker([51.963377, 7.625153], { icon: whiteMarker }).addTo(map)
-    .bindPopup('Dieser Mülleimer ist praktisch leer!');
+var orangePaper = L.AwesomeMarkers.icon({
+    icon: 'file-o',
+    markerColor: 'orange'
+});
+
+var greenPaper = L.AwesomeMarkers.icon({
+    icon: 'file-o',
+    markerColor: 'green'
+});
+
+// Create Restmüll markers
+
+L.AwesomeMarkers.Icon.prototype.options.prefix = 'glyphicon';
+
+var redRest = L.AwesomeMarkers.icon({
+    icon: 'trash',
+    markerColor: 'red'
+});
+
+var orangeRest = L.AwesomeMarkers.icon({
+    icon: 'trash',
+    markerColor: 'orange'
+});
+
+var greenRest = L.AwesomeMarkers.icon({
+    icon: 'trash',
+    markerColor: 'green'
+});
+
+// L.marker([51.962377, 7.625153], { icon: redMarker }).addTo(map)
+//     .bindPopup('Dieser Mülleimer ist komplett voll!');
 
 function init() {
-     $.ajax({
-        type : "GET",
-        dataType : "json",
-        url : "http://giv-project7.uni-muenster.de:5000/api/trash_bins",
-        success: function(data){
-           console.log(data);
-           for(i=0;i<=data.length;i++)
-            {
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "http://giv-project7.uni-muenster.de:5000/api/trash_bins?latest_measurement=true",
+        success: function (data) {
+            console.log(data);
+            for (i = 0; i < data.length; i++) {
+                var lat = data[i].latitude;
+                var lon = data[i].longitude;
+                var type = data[i].wastetype;
+                var green = data[i].green;
+                var orange = data[i].orange;
+                var red = data[i].red;
+                var height = data[i].waste_height;
+                var type = data[i].wastetype;
+                var color;
+                console.log(data[i].waste_height);
 
-           }
-     }
-});
+
+                if (height <= green) {
+                    color = "green";
+                }
+                if (height > green && height <= orange) {
+                    color = "orange";
+                }
+                if (height > orange) {
+                    color = "red";
+                }
+                console.log(color)
+
+                if (type == "GelberSack") {
+                    if (color == "green") {
+                        L.marker([lat, lon], { icon: greenSack }).addTo(map)
+                    };
+                    if (color == "orange") {
+                        L.marker([lat, lon], { icon: orangeSack }).addTo(map)
+                    };
+                    if (color == "red") {
+                        L.marker([lat, lon], { icon: redSack }).addTo(map)
+                    };
+                }
+                if (type == "Restmüll") {
+                   if (color == "green") {
+                        L.marker([lat, lon], { icon: greenRest }).addTo(map)
+                    };
+                    if (color == "orange") {
+                        L.marker([lat, lon], { icon: orangeRest }).addTo(map)
+                    };
+                    if (color == "red") {
+                        L.marker([lat, lon], { icon: redRest }).addTo(map)
+                    };
+                }
+                if (type == "Papier") {
+                   if (color == "green") {
+                        L.marker([lat, lon], { icon: greenPaper }).addTo(map)
+                    };
+                    if (color == "orange") {
+                        L.marker([lat, lon], { icon: orangePaper}).addTo(map)
+                    };
+                    if (color == "red") {
+                        L.marker([lat, lon], { icon: redPaper }).addTo(map)
+                    };            
+                }
+            }
+        }
+    });
 }
 
 init();
